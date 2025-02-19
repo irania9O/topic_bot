457e9420eb4e9e673cd60c9ffac06e8c3ca62d46
Ali Kamali <seyedalikamali1381@gmail.com>
2025-02-19 20:23:07 +0330

    Adds user creator middleware

---
+++ middlewares/add_user.py
@@ -0,0 +1,17 @@
+from typing import Any, Awaitable, Callable, Dict
+from aiogram import BaseMiddleware
+from aiogram.types import Message
+from ..db.user import add_user
+
+class UserCreatorMiddleware(BaseMiddleware):
+    def __init__(self) -> None:
+        pass
+
+    async def __call__(
+        self,
+        handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]],
+        event: Message,
+        data: Dict[str, Any]
+    ) -> Any:
+        add_user(Message.from_user.id, Message.from_user.username, Message.from_user.first_name, Message.from_user.last_name,  Message.from_user.language_code)
+        return await handler(event, data)
